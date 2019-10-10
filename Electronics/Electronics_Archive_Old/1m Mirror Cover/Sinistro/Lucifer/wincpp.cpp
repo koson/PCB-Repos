bf4440d89414d18348b3cd8811e5e80fa6507e95
Koson Trachu <koson.trachu@gmail.com>
2019-10-11 06:47:35 +0700

    add submodule

--- Electronics/Electronics_Archive_Old/1m Mirror Cover/Sinistro/Lucifer/wincpp.cpp
+++
@@ -1,6 +0,0 @@
-// wincpp.cpp : source file that includes just the standard includes
-//	Lucifer.pch will be the pre-compiled header
-//	wincpp.obj will contain the pre-compiled type information
-
-#include "wincpp.hpp"
-
